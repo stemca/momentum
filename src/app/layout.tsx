563881489333b243a64c9c21b9f92350d48ded2a
stemca <stevenmatthew.arce@gmail.com>
2025-03-25 12:04:29 -0700

    added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
 	title: "Hono | nextjs",
@@ -15,6 +16,7 @@ export default function RootLayout({
 		<html lang="en">
 			<body>{children}</body>
 			<Analytics />
+			<SpeedInsights />
 		</html>
 	);
 }
