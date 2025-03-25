9538ffa9f6979f1822a8f29d2c4821ec234cb435
stemca <stevenmatthew.arce@gmail.com>
2025-03-25 12:03:01 -0700

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
 	title: "Hono | nextjs",
@@ -13,6 +14,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body>{children}</body>
+			<Analytics />
 		</html>
 	);
 }
