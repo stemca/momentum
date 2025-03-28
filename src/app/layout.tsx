02d4c58f3315d853e2853e342b5d086a2f46c3eb
stemca <stevenmatthew.arce@gmail.com>
2025-03-28 08:39:29 -0700

    remove vercel stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Providers } from "./providers";
 
@@ -18,8 +16,6 @@ export default function RootLayout({
 			<Providers>
 				<body>{children}</body>
 			</Providers>
-			<Analytics />
-			<SpeedInsights />
 		</html>
 	);
 }
