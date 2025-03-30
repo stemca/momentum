e30678d0f6167ab11fcd0456d9b5c9877844be0a
stemca <stevenmatthew.arce@gmail.com>
2025-03-30 02:10:48 -0700

    test

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,14 +1,23 @@
+import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
-const envSchema = z.object({
-	NODE_ENV: z.enum(["development", "production"]).default("development"),
-	DATABASE_URL: z.string(),
-	DATABASE_AUTH_TOKEN: z.string().optional(), // only required in prod
-	DISCORD_CLIENT_ID: z.string(),
-	DISCORD_CLIENT_SECRET: z.string(),
-	DISCORD_CALLBACK_URL: z.string(),
+const env = createEnv({
+	server: {
+		NODE_ENV: z.enum(["development", "production"]).default("development"),
+		DATABASE_URL: z.string(),
+		DISCORD_CLIENT_ID: z.string(),
+		DISCORD_CLIENT_SECRET: z.string(),
+		DISCORD_CALLBACK_URL: z.string(),
+	},
+	runtimeEnv: {
+		DATABASE_URL: process.env.DATABASE_URL,
+		NODE_ENV: process.env.NODE_ENV,
+		DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+		DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+		DISCORD_CALLBACK_URL: process.env.DISCORD_CALLBACK_URL,
+		// NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+	},
+	skipValidation: !!process.env.SKIP_VALIDATION,
 });
-
-const env = envSchema.parse(process.env);
 
 export default env;
