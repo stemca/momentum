ff5ecd8ef5318b20287cad155d6f16379314284c
stemca <stevenmatthew.arce@gmail.com>
2025-03-30 23:21:21 -0700

    navbar says home when authenticated

--- src/app/(public)/components/navbar.tsx
+++ src/app/(public)/components/navbar.tsx
@@ -10,6 +10,7 @@ import {
 	SheetTitle,
 	SheetTrigger,
 } from "@/components/ui/sheet";
+import { getCurrentSession } from "@/services/auth/session";
 
 // const navLinks = [
 // 	{ href: "/about", label: "About" },
@@ -17,7 +18,9 @@ import {
 // 	{ href: "/pricing", label: "Pricing" },
 // ];
 
-export default function Navbar() {
+export default async function Navbar() {
+	const { user } = await getCurrentSession();
+
 	return (
 		<header className="mx-auto flex w-full max-w-screen-lg justify-between px-5 py-6">
 			<div className="flex items-center gap-2">
@@ -29,14 +32,24 @@ export default function Navbar() {
 
 			{/* Desktop Buttons */}
 			<div className="hidden h-full flex-1 items-center justify-end space-x-4 md:flex">
-				<Link href="/auth/sign-in">
-					<Button variant="link" className="text-md" size="lg">
-						Log in
-					</Button>
-				</Link>
-				<Link href="/auth/sign-up">
-					<Button>Sign Up</Button>
-				</Link>
+				{user ? (
+					<Link href="/home">
+						<Button className="text-md" size="lg">
+							Home
+						</Button>
+					</Link>
+				) : (
+					<>
+						<Link href="/auth/sign-in">
+							<Button variant="link" className="text-md" size="lg">
+								Log in
+							</Button>
+						</Link>
+						<Link href="/auth/sign-up">
+							<Button>Sign Up</Button>
+						</Link>
+					</>
+				)}
 			</div>
 
 			{/* Mobile Menu */}
@@ -52,24 +65,28 @@ export default function Navbar() {
 							<SheetTitle />
 						</SheetHeader>
 						<SheetClose asChild>
-							<Link href="/">
-								<Button variant="link" className="text-lg">
+							<Link href="/home">
+								<Button className="text-lg" variant="link">
 									Home
 								</Button>
 							</Link>
 						</SheetClose>
-						<SheetClose asChild>
-							<Link href="/auth/sign-in">
-								<Button variant="link" className="text-lg">
-									Log in
-								</Button>
-							</Link>
-						</SheetClose>
-						<SheetClose asChild>
-							<Link href="/auth/sign-up">
-								<Button size="lg">Sign Up</Button>
-							</Link>
-						</SheetClose>
+						{user === null && (
+							<>
+								<SheetClose asChild>
+									<Link href="/auth/sign-in">
+										<Button variant="link" className="text-lg">
+											Log in
+										</Button>
+									</Link>
+								</SheetClose>
+								<SheetClose asChild>
+									<Link href="/auth/sign-up">
+										<Button size="lg">Sign Up</Button>
+									</Link>
+								</SheetClose>
+							</>
+						)}
 					</SheetContent>
 				</Sheet>
 			</div>
